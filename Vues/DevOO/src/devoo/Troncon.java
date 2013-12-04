dea963c79c821598a21bb6887a008a40de19fcd7
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2013-12-04 17:31:16 +0100

    Tronçons bis

---
+++ Vues/DevOO/src/devoo/Troncon.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package devoo;
+
+/**
+ *
+ * @author tanguyhelesbeux
+ */
+public class Troncon {
+    
+    public Noeud depart;
+    public Noeud arrivee;
+    
+}
