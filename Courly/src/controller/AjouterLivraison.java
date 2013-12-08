c77101cf2f366992fd79a2a160ac5688099af749
Ianic Almeida <ianicx@gmail.com>
2013-12-08 19:29:06 +0100

    Classe AjouterLivraison

---
+++ Courly/src/controller/AjouterLivraison.java
@@ -0,0 +1,15 @@
+package controller;
+
+import javax.swing.undo.AbstractUndoableEdit;
+import javax.swing.undo.CannotUndoException;
+
+public class AjouterLivraison extends AbstractUndoableEdit {
+
+	public AjouterLivraison() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void undo() throws CannotUndoException {
+		//TODO
+	}
+}
