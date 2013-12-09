16163250e456e0b1cd8b865a4df8edb73a25076b
Ianic Almeida <ianicx@gmail.com>
2013-12-09 09:03:42 +0100

    Added classes for Undo and Redo

---
+++ Courly/src/controller/AjouterLivraisonAction.java
@@ -0,0 +1,30 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Icon;
+
+public class AjouterLivraisonAction extends AbstractAction {
+
+	public AjouterLivraisonAction() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public AjouterLivraisonAction(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AjouterLivraisonAction(String arg0, Icon arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Courly/src/controller/EnleverLivraisonAction.java
@@ -0,0 +1,30 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Icon;
+
+public class EnleverLivraisonAction extends AbstractAction {
+
+	public EnleverLivraisonAction() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public EnleverLivraisonAction(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EnleverLivraisonAction(String arg0, Icon arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
