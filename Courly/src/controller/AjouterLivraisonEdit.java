236da2f5c9cc3a5001981fd614733fa30396b98f
Ianic Almeida <ianicx@gmail.com>
2013-12-11 11:44:44 +0100

    Added undo and redo code to AjouterLivraisonEdit

--- Courly/src/controller/AjouterLivraisonEdit.java
+++ Courly/src/controller/AjouterLivraisonEdit.java
@@ -7,40 +7,48 @@ import javax.swing.undo.CannotUndoException;
 import model.Livraison;
 
 public class AjouterLivraisonEdit extends AbstractUndoableEdit {
-        
-        private static final String presentationName = "Ajouter livraison";
-        private Livraison livraison = null;
-        private ControleurFenetrePrincipale controleur = null;
 
-        public AjouterLivraisonEdit(Livraison l, ControleurFenetrePrincipale c) {
-            this.livraison = l;
-            this.controleur = c;
-     }
-        
-        @Override
-        public void undo() throws CannotUndoException {
-                //TODO
-        }
-        
-        @Override
-        public void redo() throws CannotRedoException {
-                //TODO
-        }
-        
-        @Override
-        public boolean canUndo() {
-                 //TODO
-                 return true; 
-        }
-        
-        @Override
-        public boolean canRedo() {
-                //TODO
-                return true;
-        }
-        
-        @Override
-        public String getPresentationName() {
-                return presentationName;
-        }
+	private static final String presentationName = "Ajouter livraison";
+	private Livraison livraison = null;
+	private ControleurFenetrePrincipale controleur = null;
+
+	public AjouterLivraisonEdit(Livraison l, ControleurFenetrePrincipale c) {
+		this.livraison = l;
+		this.controleur = c;
+	}
+
+	@Override
+	public void undo() throws CannotUndoException {
+		//Enlever livraison
+		controleur.shouldRemoveLivraisonAndReload(livraison);
+	}
+
+	@Override
+	public void redo() throws CannotRedoException {
+		//Ajouter livraison
+		controleur.shouldAddLivraisonAndReload(livraison);
+	}
+
+	@Override
+	public boolean canUndo() {
+		//TODO
+		if( livraison == null || controleur == null){
+			return false;
+		}
+		return true; 
+	}
+
+	@Override
+	public boolean canRedo() {
+		//TODO
+		if( livraison == null || controleur == null){
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String getPresentationName() {
+		return presentationName;
+	}
 }
