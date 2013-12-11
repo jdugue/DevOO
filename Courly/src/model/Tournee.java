c940be65e7252c701ef7e98f63fc6884415321b6
jdugue <jossdug@gmail.com>
2013-12-11 09:19:43 +0100

    tentative de fix des images

--- Courly/src/model/Tournee.java
+++ Courly/src/model/Tournee.java
@@ -4,26 +4,18 @@ import java.util.ArrayList;
 
 public class Tournee {
 	
-	protected ArrayList<Lieu> lieux;
-	public ArrayList<Lieu> getLieux() {
-		return lieux;
-	}
-
-	public void setLieux(ArrayList<Lieu> lieux) {
-		this.lieux = lieux;
-	}
-
-	public ArrayList<Trajet> getTrajets() {
-		return trajets;
-	}
-
-	public void setTrajets(ArrayList<Trajet> trajets) {
-		this.trajets = trajets;
-	}
-
+	protected ArrayList<Livraison> livraisons;
 	protected ArrayList<Trajet> trajets;
 	protected ArrayList<PlageHoraire> plagesHoraire;
-		
+	
+	public ArrayList<Livraison> getLivraisons() {
+		return livraisons;
+	}
+	
+	public void setLivraisons(ArrayList<Livraison> livraisons) {
+		this.livraisons = livraisons;
+	}
+	
 	public ArrayList<Trajet> getTrajet() {
 		return trajets;
 	}
@@ -40,9 +32,9 @@ public class Tournee {
 		this.plagesHoraire = plagesHoraire;
 	}
 	
-	public void addLivraison(Livraison livraison , Integer pos)
-	{
-		lieux.add(pos, livraison);
+	public void addLivraison(Livraison livraison){
+		livraisons.add(livraison);
+		
 	}
 
 }
