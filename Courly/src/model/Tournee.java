d9a86c7c0a60dc4c2ef2cc99527d4c2aeed4a0a6
Mael <mael.ogier.38@gmail.com>
2013-12-19 16:36:25 +0100

    fin javadoc model

--- Courly/src/model/Tournee.java
+++ Courly/src/model/Tournee.java
@@ -4,30 +4,45 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Tournee {
-	
+
+	/**
+	 * Le dépôt de départ de la Tournée 
+	 */
 	protected Depot depot;
+
+	/**
+	 * La liste des livraisons de la Tournee
+	 */
 	protected ArrayList<Livraison> livraisons;
+
+	/**
+	 * La liste des trajets de la Tournee 
+	 */
 	protected ArrayList<Trajet> trajets;
+
+	/**
+	 * La liste des PlageHoraire de la Tournee 
+	 */
 	protected ArrayList<PlageHoraire> plagesHoraire;
 
-    public Tournee() {
-    }
+	public Tournee() {
+	}
 
-    public Tournee(Tournee tournee) {
-        this.livraisons = new ArrayList<Livraison>(tournee.getLivraisons());
-        this.trajets = new ArrayList<Trajet>(tournee.getTrajets());
-        this.plagesHoraire = new ArrayList<PlageHoraire>(tournee.getPlagesHoraire());
-        this.depot = tournee.getDepot();
-    }
-	
+	public Tournee(Tournee tournee) {
+		this.livraisons = new ArrayList<Livraison>(tournee.getLivraisons());
+		this.trajets = new ArrayList<Trajet>(tournee.getTrajets());
+		this.plagesHoraire = new ArrayList<PlageHoraire>(tournee.getPlagesHoraire());
+		this.depot = tournee.getDepot();
+	}
+
 	public ArrayList<Livraison> getLivraisons() {
 		return livraisons;
 	}
-	
+
 	public void setLivraisons (ArrayList<Livraison> livraisons) {
 		this.livraisons = livraisons;
 	}
-	
+
 	public Depot getDepot() {
 		return depot;
 	}
@@ -43,20 +58,28 @@ public class Tournee {
 	public void setTrajets(ArrayList<Trajet> trajets) {
 		this.trajets = trajets;
 	}
-	
+
 	public ArrayList<PlageHoraire> getPlagesHoraire() {
 		return plagesHoraire;
 	}
-	
+
 	public void setPlagesHoraire(ArrayList<PlageHoraire> plagesHoraire) {
 		this.plagesHoraire = plagesHoraire;	
 	}
-        
-        public void removeLivraison(Livraison livraison) {
-            livraison.getPlageHoraire().livraisons.remove(livraison);
-            this.livraisons.remove(livraison);
-        }
-	
+
+	/**
+	 * Supprime une livraison de la Tournee et la supprime aussi de la liste des livraisons de la plage horaire correspondante
+	 * @param livraison La livraison à supprimer
+	 */
+	public void removeLivraison(Livraison livraison) {
+		livraison.getPlageHoraire().livraisons.remove(livraison);
+		this.livraisons.remove(livraison);
+	}
+
+	/**
+	 * Permet d'obtenir la première plage horaire de la Tournee
+	 * @return La première plage horaire de la tournée
+	 */
 	public PlageHoraire getFirstPlageHoraire () {
 		PlageHoraire plage = plagesHoraire.get(0);
 		if (plagesHoraire.size()>1){
@@ -68,7 +91,11 @@ public class Tournee {
 		}
 		return plage;
 	}
-	
+
+	/**
+	 * Ajoute une livraison à la Tournee et l'ajoute aussi à la bonne plage horaire
+	 * @param livraison : la livraison à ajouter
+	 */
 	public void addLivraison(Livraison livraison){
 		livraisons.add(livraison);	
 		livraison.getPlageHoraire().addLivraison(livraison);
